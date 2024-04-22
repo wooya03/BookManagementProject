238e49b3f64ab7a16009921059e35fb3b2e89360
Wooya <wooya_0@naver.com>
2024-04-23 00:10:12 +0900

    Service

---
+++ src/kr/ac/bms/service/BookService.java
@@ -0,0 +1,13 @@
+package kr.ac.bms.service;
+
+import java.util.ArrayList;
+import kr.ac.bms.model.Book;
+
+public interface BookService {
+    int uniqId();
+    void regist(Book book);
+    Book list(Book book);
+    ArrayList<Book> getALl();
+    void modify(Book book);
+    void remove(int id);
+}
---
+++ src/kr/ac/bms/service/BookServiceImpl.java
@@ -0,0 +1,40 @@
+package kr.ac.bms.service;
+
+import kr.ac.bms.model.Book;
+
+import java.util.ArrayList;
+
+public class BookServiceImpl implements BookService{
+    static int uniqId = 0;
+    ArrayList<Book> books = new ArrayList<>();
+
+    @Override
+    public int uniqId() {
+        return uniqId++;
+    }
+
+    @Override
+    public void regist(Book book) {
+        books.add(book);
+    }
+
+    @Override
+    public Book list(Book book) {
+        return book;
+    }
+
+    @Override
+    public ArrayList<Book> getALl() {
+        return books;
+    }
+
+    @Override
+    public void modify(Book book) {
+        books.set(book.getId(), book);
+    }
+
+    @Override
+    public void remove(int id) {
+        books.remove(id);
+    }
+}
